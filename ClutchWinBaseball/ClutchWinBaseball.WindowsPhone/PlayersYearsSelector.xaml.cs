458c345f9eb2224d7dc0f537fe60df10ad6394c3
hoovejo <joe_hoover7@hotmail.com>
2014-07-01 21:57:30 -0400

    tweak seasons lookup to match ios/android; remove background wp8

--- ClutchWinBaseball/ClutchWinBaseball.WindowsPhone/PlayersYearsSelector.xaml.cs
+++ ClutchWinBaseball/ClutchWinBaseball.WindowsPhone/PlayersYearsSelector.xaml.cs
@@ -1,9 +1,12 @@
-﻿using ClutchWinBaseball.Portable;
+﻿using ClutchWinBaseball.ItemViews;
+using ClutchWinBaseball.Portable;
 using ClutchWinBaseball.Portable.Common;
 using ClutchWinBaseball.Portable.FeatureStateModel;
 using ClutchWinBaseball.Portable.ViewModels;
+using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+
 
 namespace ClutchWinBaseball
 {
@@ -74,5 +77,65 @@ namespace ClutchWinBaseball
                 notify.IsOpen = false;
             }
         }
+
+        #region Data Visualization
+        /// <summary>
+        /// We will visualize the data item in asynchronously in multiple phases for improved panning user experience 
+        /// of large lists.  In this sample scneario, we will visualize different parts of the data item
+        /// in the following order:
+        /// 
+        ///     1) Placeholders (visualized synchronously - Phase 0)
+        ///     2) Tilte (visualized asynchronously - Phase 1)
+        ///     3) Image (visualized asynchronously - Phase 2)
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void Items_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            PlayersYearItem iv = args.ItemContainer.ContentTemplateRoot as PlayersYearItem;
+
+            if (args.InRecycleQueue == true)
+            {
+                iv.ClearData();
+            }
+            else if (args.Phase == 0)
+            {
+                iv.ShowPlaceholder(args.Item as PlayersYearsViewModel);
+
+                // Register for async callback to visualize Title asynchronously
+                args.RegisterUpdateCallback(ContainerContentChangingDelegate);
+            }
+            else if (args.Phase == 1)
+            {
+                iv.ShowTitle();
+                args.RegisterUpdateCallback(ContainerContentChangingDelegate);
+            }
+            else if (args.Phase == 2)
+            {
+                iv.ShowCategory();
+                args.RegisterUpdateCallback(ContainerContentChangingDelegate);
+            }
+            // For improved performance, set Handled to true since app is visualizing the data item
+            args.Handled = true;
+        }
+
+        /// <summary>
+        /// Managing delegate creation to ensure we instantiate a single instance for 
+        /// optimal performance. 
+        /// </summary>
+        private TypedEventHandler<ListViewBase, ContainerContentChangingEventArgs> ContainerContentChangingDelegate
+        {
+            get
+            {
+                if (_delegate == null)
+                {
+                    _delegate = new TypedEventHandler<ListViewBase, ContainerContentChangingEventArgs>(Items_ContainerContentChanging);
+                }
+                return _delegate;
+            }
+        }
+        private TypedEventHandler<ListViewBase, ContainerContentChangingEventArgs> _delegate;
+        #endregion
     }
 }
